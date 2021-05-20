14ce9de0346ac34b9fb044cc5699afbb71d3443d
Amantay <amantay.sv@gmail.com>
2021-05-21 00:05:12 +0600

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,41 @@
 $(document).ready(function () {
-  var owl = $('.owl-carousel');
+  var owl = $(".owl-carousel");
   owl.owlCarousel({
     items: 1,
     loop: true,
     dots: false,
   });
-  $('.owl-next').click(function () {
-    owl.trigger('next.owl.carousel');
+  $(".owl-next").click(function () {
+    owl.trigger("next.owl.carousel");
   });
-  $('.owl-prev').click(function () {
-    owl.trigger('prev.owl.carousel', [300]);
+  $(".owl-prev").click(function () {
+    owl.trigger("prev.owl.carousel", [300]);
   });
 });
 
-$('ul.catalog__caption').on('click', 'li:not(.catalog__tab_active)', function () {
+$("ul.catalog__caption").on("click", "li:not(.active)", function () {
   $(this)
-    .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-    .closest('div.catalog__tabs').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    .addClass("active")
+    .siblings()
+    .removeClass("active")
+    .closest("div.catalog__tabs")
+    .find("div.catalog__content")
+    .removeClass("active")
+    .eq($(this).index())
+    .addClass("active");
 });
 
 function toggleSlide(item) {
   $(item).each(function (i) {
-    $(this).on('click', function (e) {
+    $(this).on("click", function (e) {
       e.preventDefault();
-      $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-      $('.catalog-item__about').eq(i).toggleClass('catalog-item__about_active');
+      $(".catalog-item__content")
+        .eq(i)
+        .toggleClass("active");
+      $(".catalog-item__about").eq(i).toggleClass("active");
     });
   });
 }
 
-toggleSlide('.catalog-item__link');
-toggleSlide('.catalog-item__back');
+toggleSlide(".catalog-item__link");
+toggleSlide(".catalog-item__back");
